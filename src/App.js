0faac69c1c04115f2dc79d41df5660337342df87
Aaditya Pratap Singh <aaditya0821@gmail.com>
2024-09-17 20:02:11 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import HomePage from './pages/HomePage';
+import TaskPage from './pages/TaskPage';
+import UserProfilePage from './pages/UserProfilePage';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/tasks" element={<TaskPage />} />
+        <Route path="/profile" element={<UserProfilePage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
